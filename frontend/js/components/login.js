0695d14ee697f008a88c53cc9e55c08468a59bdf
remolan1213 <r.lanugon@gmail.com>
2024-04-11 13:38:32 +0800

    deleted all because sequelize didn't work out using prisma now

--- frontend/js/components/login.js
+++ frontend/js/components/login.js
@@ -4,10 +4,12 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
     
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    
+   
     try {
         const response = await axios.post('https://9c0a39ce-8d58-4d23-8bd6-5b180d54b691.mock.pstmn.io', { username, password });
         console.log(response.data); // Handle response from server
+        console.log("Login successful");
+        console.log(username, password);
     } catch (error) {
         console.error(error);
     }
